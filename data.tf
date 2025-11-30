086687216af7ddccb8176af95b1400e78ca907d4
taku <takumi.f.0308@icloud.com>
2025-11-30 23:31:06 +0900

    fix app-sg

---
+++ data.tf
@@ -0,0 +1,3 @@
+data "aws_prefix_list" "s3_pl" {
+  name = "com.amazonaws.ap-northeast-1.s3"
+}
--- security_group.tf
+++ security_group.tf
@@ -50,6 +50,42 @@ resource "aws_security_group" "app-sg" {
   }
 }
 
+resource "aws_security_group_rule" "app-in-tcp" {
+  security_group_id        = aws_security_group.app-sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3000
+  to_port                  = 3000
+  source_security_group_id = aws_security_group.web-sg.id
+}
+
+resource "aws_security_group_rule" "app-out-http" {
+  security_group_id = aws_security_group.app-sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
+}
+
+resource "aws_security_group_rule" "app-out-https" {
+  security_group_id = aws_security_group.app-sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
+}
+
+resource "aws_security_group_rule" "app-out-tpc" {
+  security_group_id        = aws_security_group.app-sg.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.db-sg.id
+}
+
 resource "aws_security_group" "opmng-sg" {
   name        = "${var.project}-${var.environment}-opmng-sg"
   description = "opmng-sg"
