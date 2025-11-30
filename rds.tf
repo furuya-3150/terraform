bbed66222ffaeb8fecf27106b492ad22f8aa2738
taku <takumi.f.0308@icloud.com>
2025-12-01 08:08:26 +0900

    option, subnet group

--- rds.tf
+++ rds.tf
@@ -11,3 +11,23 @@ resource "aws_db_parameter_group" "mysql_standalone_parametergroup" {
     value = "utf8mb4"
   }
 }
+
+resource "aws_db_option_group" "mysql_standalone_optiongroup" {
+  name                 = "${var.project}-${var.environment}-mysql-standalone-optiongroup"
+  engine_name          = "mysql"
+  major_engine_version = "8.0"
+}
+
+resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
+  name = "${var.project}-${var.environment}-mysql-standalone-subnetgroup"
+  subnet_ids = [
+    aws_subnet.private_subnet-1a.id,
+    aws_subnet.private_subnet-1c.id
+  ]
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysql-standalone-subnetgroup"
+    project = var.project
+    Env     = var.environment
+  }
+}
