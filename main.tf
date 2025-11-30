ecb8efeb863df038ef8bf2f422fca26a7af53941
taku <takumi.f.0308@icloud.com>
2025-11-30 19:06:08 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
