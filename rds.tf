550d74ca1c2fb400b61d4e5892222c01e2871f28
taku <takumi.f.0308@icloud.com>
2025-12-01 08:23:16 +0900

    random string

--- rds.tf
+++ rds.tf
@@ -1,3 +1,4 @@
+# parameter group
 resource "aws_db_parameter_group" "mysql_standalone_parametergroup" {
   name   = "${var.project}-${var.environment}-mysql-standalone-parametergroup"
   family = "mysql8.0"
@@ -12,12 +13,14 @@ resource "aws_db_parameter_group" "mysql_standalone_parametergroup" {
   }
 }
 
+# option group
 resource "aws_db_option_group" "mysql_standalone_optiongroup" {
   name                 = "${var.project}-${var.environment}-mysql-standalone-optiongroup"
   engine_name          = "mysql"
   major_engine_version = "8.0"
 }
 
+# subnet group
 resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
   name = "${var.project}-${var.environment}-mysql-standalone-subnetgroup"
   subnet_ids = [
@@ -30,4 +33,10 @@ resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
     project = var.project
     Env     = var.environment
   }
+}
+
+# db instance
+resource "random_string" "db_password" {
+  length  = 16
+  special = false
 }
