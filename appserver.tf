130d4902e716ea549a176cfcbe2fb0529ed74247
taku <takumi.f.0308@icloud.com>
2025-12-02 08:19:13 +0900

    add aws_instance to appserver

--- appserver.tf
+++ appserver.tf
@@ -10,3 +10,21 @@ resource "aws_key_pair" "keypair" {
     Env     = var.environment
   }
 }
+
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public_subnet-1a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [
+    aws_security_group.app-sg.id,
+    aws_security_group.opmng-sg.id,
+  ]
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-keypair"
+    project = var.project
+    Env     = var.environment
+    Type    = "app"
+  }
+}
