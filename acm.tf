8505acc0762bf2ca7d9728db8056b708f7b7659a
taku <takumi.f.0308@icloud.com>
2025-12-04 23:22:04 +0900

    mod acm.tf

--- acm.tf
+++ acm.tf
@@ -1,6 +1,6 @@
 # acm
 resource "aws_acm_certificate" "tokyo_region" {
-  domain_name       = "${var.domain}"
+  domain_name       = var.domain
   validation_method = "DNS"
 
   tags = {
@@ -14,4 +14,26 @@ resource "aws_acm_certificate" "tokyo_region" {
   }
 
   depends_on = [aws_route53_zone.route53_zone]
+}
+
+resource "aws_route53_record" "route53_acm_dns_resolve" {
+  for_each = {
+    for dvo in aws_acm_certificate.tokyo_region.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  allow_overwrite = true
+  zone_id         = aws_route53_zone.route53_zone.id
+  name            = each.value.name
+  type            = each.value.type
+  ttl             = 600
+  records         = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "cert_valid" {
+  certificate_arn         = aws_acm_certificate.tokyo_region.arn
+  validation_record_fqdns = [for record in aws_route53_record.route53_acm_dns_resolve : record.fqdn]
 }
